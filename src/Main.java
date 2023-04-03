892f4d8d242bc29b1f195f82215e1f596e3187a2
Zergi4 <ksyssau@ya.ru>
2023-04-03 17:10:50 +0300

    4 задача

--- src/Main.java
+++ src/Main.java
@@ -9,5 +9,8 @@ public class Main {
         for (int i = 0; i < 17; i=i+2) {
             System.out.println(" " + i);
         }
+        for (int i = 10; i >= -10; i--) {
+            System.out.println(" " + i);
+        }
         }
     }
